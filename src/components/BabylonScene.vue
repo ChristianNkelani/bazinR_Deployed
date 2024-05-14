16a63bcf847fa5a24dd691c8505653c39f71f89d
ChristianNkelani <christian.n.hiram@gmail.com>
2024-05-14 05:25:45 +0100

    first commit

---
+++ src/components/BabylonScene.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Experience1 } from '../scenes/Experience1';
+
+export default defineComponent({
+  name: 'BabylonScene',
+  
+  mounted(){
+    const canvas = document.querySelector('canvas')!;
+    new Experience1(canvas);
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  canvas{
+    width: 100%;
+    height: 100%;
+  }
+</style>
